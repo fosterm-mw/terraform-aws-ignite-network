0f44794ee7ca3959360db24831adac51be599049
fosterm-mw <michael.foster@mavenwave.com>
2022-12-06 12:35:33 -0600

    adding more outputs for eks

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,11 @@ output "network_id" {
   value = aws_vpc.network.id
 }
 
+output "private_subnet_id" {
+  value = aws_subnet.private-subnet.id
+}
+
+output "failover_private_subnet_id" {
+  value = aws_subnet.private-failover-subnet.id
+}
+
